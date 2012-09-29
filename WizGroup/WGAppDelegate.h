d4900081c952d48f0ad15921b4450befa8ebf20b
dzpqzb <yishuiliunian@gmail.com>
2012-09-29 10:35:59 +0800

    Initial Commit

---
+++ WizGroup/WGAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  WGAppDelegate.h
+//  WizGroup
+//
+//  Created by wiz on 12-9-29.
+//  Copyright (c) 2012年 cn.wiz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WGAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ WizGroup/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  WizGroup
+//
+//  Created by wiz on 12-9-29.
+//  Copyright (c) 2012年 cn.wiz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "WGAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([WGAppDelegate class]));
+    }
+}
